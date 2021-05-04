cba9a7b811212f5f7d24db2472f7f32e23ecfe64
lex0819 <lexmolnar@gmail.com>
2021-05-04 21:39:41 +0300

    gulp-file-include

---
+++ smartgrid.js
@@ -0,0 +1,28 @@
+module.exports = {
+   filename: "smart-grid",
+   outputStyle: "less",
+   columns: 12,
+   offset: "30px",
+   mobileFirst: false,
+   container: {
+      maxWidth: "1170px",
+      fields: "30px"
+   },
+   breakPoints: {
+      md: {
+         width: "992px",
+      },
+      sm: {
+         width: "768px",
+         fields: "15px"
+      },
+      xs: {
+         width: "576px",
+         fields: "10px"
+      },
+      xxs: {
+         width: "400px",
+      },
+   },
+   tab: "   ",
+};
